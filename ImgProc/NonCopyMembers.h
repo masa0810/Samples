9ce284d5999e378a7cc789ef1bcd9e6cf162aa33
masa0810 <masahiro0810@gmail.com>
2018-07-17 16:38:55 +0900

    Create NonCopyMembers.h

---
+++ ImgProc/NonCopyMembers.h
@@ -0,0 +1,170 @@
+#pragma once
+
+// 全ての警告を抑制
+MSVC_ALL_WARNING_PUSH
+
+#include <cstddef>
+
+#include <tuple>
+
+// 警告抑制解除
+MSVC_WARNING_POP
+
+/// <summary>
+/// 共通ユーティリティ
+/// </summary>
+namespace commonutility {
+
+/// <summary>
+/// コピーしないメンバー定義のためのクラス
+/// </summary>
+/// <remarks>
+/// <para>std::mutex などコピーやムーブが禁止されているクラスをメンバーとして持った場合、</para>
+/// <para>コピーコンストラクタなどのコンパイラ定義のメソッドを自分で定義しなくてはならない。</para>
+/// <para>その定義を簡略化するためのクラス。</para>
+/// </remarks>
+/// @tparam Types メンバータイプ
+template <typename... Types>
+class NonCopyMembers_ {
+  //! タプルタイプ
+  using TupleType = std::tuple<Types...>;
+  //! メンバ
+  mutable TupleType m_members = {};
+
+ public:
+  //! メンバ数
+  static constexpr auto NumOfMembers = std::tuple_size<TupleType>::value;
+  //! メンバタイプ
+  //!@tparam I メンバインデックス
+  template <std::size_t I>
+  using NcMemberType_ = std::tuple_element_t<I, TupleType>;
+  //! メンバタイプ(インデックス0の要素)
+  using NcMemberType = std::tuple_element_t<0, TupleType>;
+
+#pragma region デフォルトメソッド定義
+
+  /// <summary>
+  /// デフォルトコンストラクタ
+  /// </summary>
+  NonCopyMembers_() = default;
+
+  /// <summary>
+  /// コピーコンストラクタ
+  /// </summary>
+  NonCopyMembers_(const NonCopyMembers_&) {}
+
+  /// <summary>
+  /// 代入演算子
+  /// </summary>
+  NonCopyMembers_& operator=(const NonCopyMembers_&) { return *this; }
+
+  /// <summary>
+  /// ムーブコンストラクタ
+  /// </summary>
+  NonCopyMembers_(NonCopyMembers_&&) {}
+
+  /// <summary>
+  /// ムーブ代入演算子
+  /// </summary>
+  NonCopyMembers_& operator=(NonCopyMembers_&&) { return *this; }
+
+  /// <summary>
+  /// デストラクタ
+  /// </summary>
+  virtual ~NonCopyMembers_() = default;
+
+#pragma endregion
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// @tparam I メンバインデックス
+  template <std::size_t I>
+  constexpr NcMemberType_<I>& NcMember() noexcept {
+    return std::get<I>(m_members);
+  }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// <remarks>インデックス0の要素専用</remarks>
+  constexpr NcMemberType& NcMember() noexcept { return std::get<0>(m_members); }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// <remarks>const用</remarks>
+  /// @tparam I メンバインデックス
+  template <std::size_t I>
+  constexpr const NcMemberType_<I>& NcMember() const noexcept {
+    return std::get<I>(m_members);
+  }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// <remarks>
+  /// <para>インデックス0の要素専用</para>
+  /// <para>const用</para>
+  /// </remarks>
+  constexpr const NcMemberType& NcMember() const noexcept { return std::get<0>(m_members); }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// <remarks>const用</remarks>
+  /// @tparam I メンバインデックス
+  template <std::size_t I>
+  constexpr NcMemberType_<I>& NcMemberMutable() const noexcept {
+    return std::get<I>(m_members);
+  }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// <remarks>
+  /// <para>インデックス0の要素専用</para>
+  /// <para>mutable アクセス用</para>
+  /// </remarks>
+  constexpr NcMemberType& NcMemberMutable() const noexcept { return std::get<0>(m_members); }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// @tparam T メンバタイプ
+  template <typename T>
+  constexpr T& NcMember() noexcept {
+    return std::get<T>(m_members);
+  }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// <remarks>const用</remarks>
+  /// @tparam T メンバタイプ
+  template <typename T>
+  constexpr const T& NcMember() const noexcept {
+    return std::get<T>(m_members);
+  }
+
+  /// <summary>
+  /// メンバアクセス
+  /// </summary>
+  /// <returns>メンバへの参照</returns>
+  /// <remarks>mutable アクセス用</remarks>
+  /// @tparam T メンバタイプ
+  template <typename T>
+  constexpr T& NcMemberMutable() const noexcept {
+    return std::get<T>(m_members);
+  }
+};
+
+}  // namespace commonutility
